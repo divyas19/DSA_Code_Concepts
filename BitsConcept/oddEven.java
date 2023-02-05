9c7235576f30dc2b0d9c748b3ebda8988139b966
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-02-05 15:48:13 +0530

    Create oddEven.java

---
+++ BitsConcept/oddEven.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[])
+    {
+        int a = 90;
+        if((a&1) == 1)
+        {
+            System.out.println("ODD");
+        }
+        else
+        {
+            System.out.println("EVEN");
+        }
+    }
+}
