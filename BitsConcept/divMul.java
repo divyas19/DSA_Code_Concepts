e675f8c738272f8dbf025afb52d6d27fb9a20df3
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-02-05 15:42:18 +0530

    Create divMul.java

---
+++ BitsConcept/divMul.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[])
+    {
+        int a = 5;
+        //Divide by 2
+        System.out.println(a>>1);
+        //Multiply by 2
+        System.out.println(a<<1);
+    }
+}
