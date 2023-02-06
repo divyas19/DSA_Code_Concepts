cf44cf801e89ad72fa299fb4535f1e1bd4b44b43
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-02-06 11:03:14 +0530

    Update trailingZerosInFactorial.java

--- BasicCode/trailingZerosInFactorial.java
+++ BasicCode/trailingZerosInFactorial.java
@@ -1,10 +1,21 @@
+import java.util.*;
+import java.io.*;
+
 class Solution{
-    static int trailingZeros(int N){
+    static int trailingZeros(int N)
+    {
         int ct =0;
         for(int i = 5;N/i>0;i*=5)
         {
             ct+=N/i;
         }
         return ct;
+    }
+    public static void main (String[]args)
+    {
+    Scanner sc = new Scanner (System.in);
+    System.out.println ("Enter No");
+    int N = sc.nextInt ();
+    System.out.println (trailingZeros (N));
     }
 }
