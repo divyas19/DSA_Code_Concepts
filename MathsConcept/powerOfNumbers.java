2f80f11ea66af271cc9043a08c6b84a5650e638e
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-02-06 16:14:42 +0530

    Create powerOfNumbers.java

---
+++ MathsConcept/powerOfNumbers.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+        
+    static long power(int N,int R)
+    {
+        //Your code here
+        long m = 1000000007;
+        long v = N;
+        long p = R;
+        long res =1;
+       while(p>0)
+       {
+           if((p&1) !=0)
+           {
+               res=(res*v)%m;
+               p= p-1;
+           }
+           else
+           {
+               v = (v*v)%m;
+               p=p/2;
+           }
+       }
+        return res;
+    }
+    
+    public static void main(String args[])
+    {
+        System.out.println(power(12,21));
+    }
+
+}
