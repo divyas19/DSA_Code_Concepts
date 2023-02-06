a9c9e0885713fa56d5d63b500e3e531d25cd504e
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-02-06 12:22:35 +0530

    Create gcd.java

---
+++ MathsConcept/gcd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(gcd(24,48));
+	}
+	
+	static int gcd(int a, int b)
+	{
+	   if(b==0)
+	   return a;
+	   
+	   return gcd(b,a%b);
+	}
+}
