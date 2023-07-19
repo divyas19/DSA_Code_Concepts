59dde7ec86615ea648a31a1cdaf35c1b689935f0
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-07-19 16:12:26 +0530

    Create towerOfHanoi.java

---
+++ Recursion/towerOfHanoi.java
@@ -0,0 +1,18 @@
+class tohSolution{
+    public static void toh(int n, String src, String help, String dest) {
+        if(n==1){
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+        return;
+        }
+        
+        toh(n-1,src,dest,help);
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+        toh(n-1,help,src,dest);
+    }
+    
+     public static void main(String[] args) 
+     {
+         int n = 2;
+         toh(n,"A","B","C");
+     }
+}
