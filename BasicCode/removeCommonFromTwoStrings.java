36bbabe32806c4562bf709269aa1c7f1b54b4857
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-08-03 00:39:34 +0530

    Update removeCommonFromTwoStrings.java

--- BasicCode/removeCommonFromTwoStrings.java
+++ BasicCode/removeCommonFromTwoStrings.java
@@ -1,3 +1,109 @@
+//Remove common from both string and concatenate, if all are common return -1.
+
+    public static String concatenatedString(String s1,String s2)
+    {
+        HashMap<Character,Integer> h1 = new HashMap<>();
+        HashMap<Character,Integer> h2 = new HashMap<>();
+    
+        
+        for(int i=0;i<s1.length();i++)
+	{
+            char ch = s1.charAt(i);
+            h1.put(ch,1);
+        }
+        
+        
+        for(int i=0;i<s2.length();i++)
+	{
+            char ch = s2.charAt(i);
+            h2.put(ch,1);
+        }
+        
+        String con = s1+s2;
+        StringBuilder res = new StringBuilder();
+        int n = con.length();
+        
+        for(int i=0;i<n;i++)
+	{
+            char ch = con.charAt(i);
+            if(!h1.containsKey(ch) && h2.containsKey(ch) || !h2.containsKey(ch) && h1.containsKey(ch))
+	    {
+                
+                res.append(ch);
+                
+            }
+            
+        }
+        
+        return res.toString().isEmpty()?"-1":res.toString();
+    }
+}
+____________________________________________________________________________________________________________
+
+    public static String concatenatedString(String s1,String s2)
+    {
+          String ans2 = "";
+          String ans1="";
+          String not="";
+	
+        HashMap<Character,Integer> hm1 = new HashMap<Character,Integer>();
+        for(int i=0; i < s1.length(); i++)
+        {
+            char ch = s1.charAt(i);
+            if(hm1.containsKey(ch))
+            {
+                hm1.put(ch,hm1.get(ch)+1);
+            }
+            else
+            {
+                hm1.put(ch,1);
+            }
+        }
+        
+        for(int j=0; j < s2.length(); j++)
+        {
+            char ch = s2.charAt(j);
+            if(hm1.containsKey(ch))
+            {
+                hm1.put(ch,0);
+                not+=ch;
+            }
+            else
+            {
+                ans2+=ch;
+            }
+        }
+        
+        if(hm1.size()==0)
+        {
+            return "-1";
+        }
+        
+         for(int i=0; i < s1.length(); i++)
+         {
+             String check = String.valueOf(s1.charAt(i));
+             if(!not.contains(check))
+             {
+                ans1+=check;
+             }
+         }
+        
+        
+        String ans = ans1+ans2;
+       
+      
+        return ans;
+    }     
+
+/*
+For Input: 
+abeff
+abcd
+Output: 
+effcd
+*/
+____________________________________________________________________________________________________________
+
 //Form a Single String from Two Strings by removing the common attributes.
 
 import java.util.*;
@@ -48,11 +154,6 @@ class Main
       
   }
 }
-
-
-
-
-
 
 /*
 1
