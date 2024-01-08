8aaa9f26fcc266b20d74e31fe1c4bbda7e8d5350
Divya Suman <73398403+divyas19@users.noreply.github.com>
2024-01-08 23:47:52 +0530

    Update permutationOfString.java

--- Recursion/permutationOfString.java
+++ Recursion/permutationOfString.java
@@ -119,3 +119,68 @@ public class Main {
 o/p
 [aaa]
 */
+
+_____________________________________________________________________________________________________________________________________________
+public class Solution {
+   public static void sol(String str, int idx, String ns) {
+       if(idx==str.length()) {
+           System.out.println(ns);
+           return;
+       }
+      char c = str.charAt(idx);
+      String cCaps = String.valueOf(c).toUpperCase();
+       sol(str,idx+1,ns+c);
+       sol(str,idx+1,ns+cCaps);
+   }
+   public static void main(String args[]) {
+       String str = "ab";
+       sol(str, 0, "");
+   }
+}
+
+/*
+o/p
+ab
+aB
+Ab
+AB
+*/
+
+_____________________________________________________________________________________________________________________________________________
+public class Solution {
+    
+public static int ct=1;
+
+  public static void printPermutation(String str,  String perm) {
+      if(str.length() == 0) {
+          System.out.print(ct++);
+          System.out.println(" "+ perm);
+          return;
+      }
+      
+      for(int i=0; i<str.length(); i++) {
+          char currChar = str.charAt(i);
+          String currCharCaps = String.valueOf(currChar).toUpperCase();
+          String newStr = str.substring(0, i) + str.substring(i+1);
+          printPermutation(newStr,  perm+currChar);
+          printPermutation(newStr,  perm+currCharCaps);
+      }
+  }
+  
+  public static void main(String args[]) {
+      String str = "ab";
+      printPermutation(str,"");
+  }
+}
+
+/*
+o/p
+1 ab
+2 aB
+3 Ab
+4 AB
+5 ba
+6 bA
+7 Ba
+8 BA
+*/
