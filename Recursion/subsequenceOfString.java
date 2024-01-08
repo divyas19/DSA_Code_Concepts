e69519a80b23f1e9cd685cfd1915a0ce66c94954
Divya Suman <73398403+divyas19@users.noreply.github.com>
2024-01-08 23:20:51 +0530

    Update subsequenceOfString.java

--- Recursion/subsequenceOfString.java
+++ Recursion/subsequenceOfString.java
@@ -30,3 +30,32 @@ b
 c
 
 */
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   public static void sol(String str, int idx, String ns) {
+       if(idx==str.length()-1) {
+           System.out.println(ns+str.charAt(str.length()-1));
+           return;
+       }
+      char c = str.charAt(idx);
+       sol(str,idx+2,ns+c+"_");
+       sol(str,idx+2,ns+c);
+   }
+   public static void main(String args[]) {
+       String s = "abc";
+       String str = "";
+       for(int i=0; i < s.length()-1;i++)
+       {
+           str =  str + s.charAt(i) + "_";
+       }
+        str =  str + s.charAt(s.length()-1);
+       sol(str, 0, "");
+   }
+}
+
+// O/P
+// a_b_c
+// a_bc
+// ab_c
+// abc
