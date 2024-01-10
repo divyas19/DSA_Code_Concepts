bbd191c2a71aed9218e2cf84627bada080679e5c
Divya Suman <73398403+divyas19@users.noreply.github.com>
2024-01-10 16:35:46 +0530

    Update QuickSort.java

--- Algorithm/QuickSort.java
+++ Algorithm/QuickSort.java
@@ -1,54 +1,57 @@
 import java.util.*;
 import java.io.*;
 
-class Main{
+class Solution {
     
-    public static void swap(int a[], int m, int n)
+    public static void swap(int a[], int f, int s)
     {
-        int t = a[m];
-        a[m]=a[n];
-        a[n] = t;
+        int t = a[f];
+        a[f] = a[s];
+        a[s] = t;
     }
     
-    public static int partition(int a[], int l, int h)
+    
+    public static int partition(int a[],int l, int h)
     {
-        int pivot = a[l];
-        int i =l;
-        int j= h;
-        while(i<j)
-        {
-            while(a[i]<=pivot)
-            i++;
-            
-            while(a[j]>pivot)
-            j--;
-            
-            if(i<j)
-            swap(a,i,j);
+        int pivot = a[h];
+        int i = l - 1;
+
+        for (int j = l; j < h; j++) {
+            if (a[j] < pivot) {
+                i++;
+                swap(a,i,j);
+            }
         }
-        swap(a,j,l);
-        
-        return j;
+
+        i++;
+        // Swap pivot element with the element at index i
+        // int temp = a[i];
+        // a[i] = pivot;
+        // a[h] = temp;
+        swap(a,i,h);
+
+        return i;
     }
+
     
-    public static void quicksort(int a[], int l, int h)
+    
+    public static void quickSort(int a[] , int l , int h)
     {
         if(l<h)
         {
-            int pivot = partition(a,l,h);
-            quicksort(a,l,pivot-1);
-            quicksort(a,pivot+1,h);
+        int pivot = partition(a,l,h);
+        quickSort(a,l,pivot-1);
+        quickSort(a,pivot+1,h);
         }
     }
     
-    public static void main(String args[])
-    {
-        int a[] = {2,5,4,3,6};
-        int l=0;int h =a.length-1;
-        quicksort(a,l,h);
-        for(int i =0; i < a.length;i++)
-        {
-            System.out.print(a[i]);
-        }
+    public static void main(String[] args) {
+        int a[] = {3,-1,0,9,8};
+        quickSort(a,0,a.length-1);
+        for(int i =0; i < a.length; i++)
+        System.out.print(a[i]+" ");
     }
 }
+
+//O/P
+//-1 0 3 8 9 
