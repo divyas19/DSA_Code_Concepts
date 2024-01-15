95df267ee91e92297c2389cd5025e3abea95953a
Divya Suman <73398403+divyas19@users.noreply.github.com>
2024-01-15 15:39:56 +0530

    Update Other concepts.java

--- Tree/Other concepts.java
+++ Tree/Other concepts.java
@@ -137,3 +137,38 @@ Diameter of Tree - Approach2 O(N)
        
 //In main method
 //System.out.println(tree.dia2(root).d);
+
+
+
+
+
+
+
+Subtree of another tree
+
+public boolean isIdentical(TreeNode root,TreeNode subRoot){
+       if(subRoot == null && root == null){
+           return true;
+       }
+       if(root == null || subRoot == null){
+           return false;
+       }
+       if(root.val == subRoot.val){
+           return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+       }
+       return false;
+   }
+  
+   public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+       if(subRoot == null){
+           return true;
+       }
+       if(root == null){
+           return false;
+       }
+       if(isIdentical(root, subRoot)){
+           return true;
+       }
+       return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+   }
+
