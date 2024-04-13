6f0859a1166ff737ed75fe024ae22130b69aa8a1
Divya Suman <73398403+divyas19@users.noreply.github.com>
2024-04-13 16:09:38 +0530

    Update Kadane'sAlgorithm.java

--- Algorithm/Kadane'sAlgorithm.java
+++ Algorithm/Kadane'sAlgorithm.java
@@ -1,4 +1,4 @@
-//Find the contiguous sub-array which has the maximum sum.
+//Find the contiguous sub-array sum which has the maximum sum.
 
 import java.io.*;
 import java.util.*;
@@ -20,4 +20,42 @@ public class Main
 		System.out.println(ans);
 	}
 
+}
+
+
+//Find the contiguous sub-array which has the maximum sum.
+
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        int a[]= {1,2,-4,5,6};
+        int ans = Integer.MIN_VALUE;
+        int cur= 0;
+        int start = 0;
+        int end = 0;
+        
+        for(int i = 0; i < a.length; i++)
+        {
+            cur+= a[i];
+            
+            if(cur > ans) {
+                ans = cur;
+                end = i;
+            }
+            
+            if(cur< 0) {
+                cur = 0;
+                start = i + 1;
+            }
+        }
+        
+        System.out.println("Maximum Sum: " + ans);
+        System.out.print("Subarray with Maximum Sum: ");
+        for (int i = start; i <= end; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
