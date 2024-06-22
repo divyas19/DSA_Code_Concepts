06d675f8e96382c2a0ce415db8d5209d8e673cd5
Divya Suman <73398403+divyas19@users.noreply.github.com>
2024-06-22 18:05:22 +0530

    Update permutationOfString.java

--- Recursion/permutationOfString.java
+++ Recursion/permutationOfString.java
@@ -69,7 +69,51 @@ o/p
 */
 
 _____________________________________________________________________________________________________________________________________________ 
+
+Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+Return a list of all possible strings we could create. Return the output in any order.
    
+Example 1:
+Input: s = "a1b2"
+Output: ["a1b2","a1B2","A1b2","A1B2"]
+   
+Example 2:
+Input: s = "3z4"
+Output: ["3z4","3Z4"]
+
+Constraints:
+1 <= s.length <= 12
+s consists of lowercase English letters, uppercase English letters, and digits.   
+
+
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, s, 0);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String s, int index) {
+        if (index == s.length()) {
+            result.add(s);
+            return;
+        }
+
+        char c = s.charAt(index);
+        if (Character.isLetter(c)) {
+            backtrack(result, s.substring(0, index) + Character.toLowerCase(c) + s.substring(index + 1), index + 1);
+            backtrack(result, s.substring(0, index) + Character.toUpperCase(c) + s.substring(index + 1), index + 1);
+        } else {
+            backtrack(result, s, index + 1);
+        }
+    }
+}
+
+
+
+_____________________________________________________________________________________________________________________________________________ 
+
+
 //Print all cases permutations
    
 import java.util.*;
