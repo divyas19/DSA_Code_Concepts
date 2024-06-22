6bb86491f19782e2682afab82647000faca9f6f1
Divya Suman <73398403+divyas19@users.noreply.github.com>
2024-06-22 13:57:36 +0530

    Update towerOfHanoi.java

--- Recursion/towerOfHanoi.java
+++ Recursion/towerOfHanoi.java
@@ -1,18 +1,24 @@
-class tohSolution{
+class tohSolution {
+    static int count = 0;
+
     public static void toh(int n, String src, String help, String dest) {
-        if(n==1){
-        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
-        return;
+        if (n == 1) {
+            count++;
+            System.out.println("Step " + count + ": Transfer disk " + n + " from " + src + " to " + dest);
+            return;
         }
-        
-        toh(n-1,src,dest,help);
-        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
-        toh(n-1,help,src,dest);
+
+        toh(n-1, src, dest, help);
+
+        count++;
+        System.out.println("Step " + count + ": Transfer disk " + n + " from " + src + " to " + dest);
+
+        toh(n-1, help, src, dest);
     }
-    
-     public static void main(String[] args) 
-     {
-         int n = 2;
-         toh(n,"A","B","C");
-     }
+
+    public static void main(String[] args) {
+        int n = 2;  
+        toh(n, "A", "B", "C");
+        System.out.println("Total number of steps: " + count);
+    }
 }
